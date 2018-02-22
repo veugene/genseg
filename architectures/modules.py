To fix bad gradient and the potential for silent errors when passed a bad input patch size, input padding has been changed and output spatial size is again fixed to being equal to that of the input. Upsapling made generic for ND.

architectures/modules.py
@@ -15,16 +15,16 @@ def _to_cuda(x, device=None):
 class _overlap_tile_function(torch.autograd.Function):
     @staticmethod
     def _patch_slice_iterator(input_patch_size, output_patch_size,
-                              input_size, output_size):
+                              input_size):
         lost = np.subtract(input_patch_size, output_patch_size)
         ndim = len(input_patch_size)+2
         
         #Iterator over indices in an image, for sourcing or placing patches.
-        def index_iterator(size, offset=None):
+        def index_iterator(offset=None):
             if offset is None:
                 offset = [0]*(ndim-2)
             def recursive_idx(dim, idx):
-                for i in range(0, size[dim]+offset[dim-2],
+                for i in range(0, input_size[dim]+offset[dim-2],
                                output_patch_size[dim-2]):
                     idx[dim] = i
                     if dim < ndim-1:
@@ -45,8 +45,8 @@ class _overlap_tile_function(torch.autograd.Function):
             return indices
     
         # Process input patch-wise.
-        iter_input = index_iterator(size=input_size, offset=lost//2)
-        iter_output = index_iterator(size=output_size)
+        iter_input = index_iterator(offset=lost//2)
+        iter_output = index_iterator()
         for idx_i, idx_o in zip(iter_input, iter_output):
             sl_i = get_patch_slices(idx_i, input_patch_size)
             sl_o = get_patch_slices(idx_o, output_patch_size)
@@ -62,8 +62,7 @@ class _overlap_tile_function(torch.autograd.Function):
         new_shape = list(input_size[:2])
         for i in range(ndim-2):
             pad0 = lost[i]//2
-            pad1 = input_patch_size[i] \
-                   - (input_size[i+2]+pad0)%output_patch_size[i]
+            pad1 = lost[i]-pad0
             padded_length = pad0 + input_size[i+2] + pad1
             new_shape.append(int(padded_length))
                             
@@ -83,8 +82,7 @@ class _overlap_tile_function(torch.autograd.Function):
     
     @staticmethod
     def forward(ctx, input, model, input_patch_size, output_patch_size,
-                out_channels, output_size=None, use_gpu=False, device=None,
-                *args):
+                out_channels, use_gpu=False, device=None, *args):
         ndim = len(input_patch_size)+2
         input_size = input.size()
         
@@ -102,12 +100,8 @@ class _overlap_tile_function(torch.autograd.Function):
         input_padded[indices] = input
         
         # Create output image buffer.
-        if output_size is None:
-            output_size = list(input_size)
-            output_size[1] = out_channels
-        elif callable(output_size):
-            output_size = (input_size[0], out_channels) \
-                          + output_size(input_size[2:])
+        output_size = list(input_size)
+        output_size[1] = out_channels
         output = Variable(torch.zeros(*tuple(output_size)).float(),
                           requires_grad=True)
         if use_gpu:
@@ -118,19 +112,14 @@ class _overlap_tile_function(torch.autograd.Function):
         output_patch_list = []
         iter_type = _overlap_tile_function._patch_slice_iterator
         for sl_i, sl_o in iter_type(input_patch_size, output_patch_size,
-                                    input_size, output_size):
+                                    input_size):
             input_patch = Variable(input_padded[sl_i].data,
                                    requires_grad=True)
             if use_gpu:
                 input_patch = _to_cuda(input_patch, device)
             with torch.enable_grad():
                 output_patch = model(input_patch)
-            sl_p = [slice(None, None)]*2
-            for dim in range(2, ndim):
-                stop = min(output.size()[dim] - sl_o[dim].start,
-                           output_patch.size()[dim])
-                sl_p.append(slice(0, stop))
-            output[sl_o] = output_patch[sl_p]
+            output[sl_o] = output_patch
             output_patch_list.append(output_patch)
             input_patch_list.append(input_patch)
             
@@ -146,7 +135,6 @@ class _overlap_tile_function(torch.autograd.Function):
         ctx.use_gpu = use_gpu
         ctx.device = device
         ctx.input_size = input_size
-        ctx.output_size = output_size
 
         return output
     
@@ -169,26 +157,15 @@ class _overlap_tile_function(torch.autograd.Function):
         iter_type = _overlap_tile_function._patch_slice_iterator
         for i, (sl_i, sl_o) in enumerate(iter_type(ctx.input_patch_size,
                                                    ctx.output_patch_size,
-                                                   ctx.input_size,
-                                                   ctx.output_size)):
+                                                   ctx.input_size)):
             input_patch = ctx.input_patch_list[i]
             output_patch = ctx.output_patch_list[i]
             output_grad_patch = output_grad[sl_o]
-            
-            # Pad output_grad_patch with zeroes as necessary, to fit
-            # expected output patch size.
-            limits = [slice(0, s) for s in output_grad_patch.size()]
-            size = output_grad_patch.size()[:2]+ctx.output_patch_size
-            output_grad_patch_padded = Variable(torch.zeros(*size).float())
-            if ctx.use_gpu:
-                output_grad_patch_padded = _to_cuda(output_grad_patch_padded,
-                                                    ctx.device)
-            output_grad_patch_padded[limits] = output_grad_patch
 
             # Get gradients
             grad_patch = torch.autograd.grad(output_patch,
                                              (input_patch,)+tuple(params),
-                                             output_grad_patch_padded,
+                                             output_grad_patch,
                                              retain_graph=True)
             input_grad_patch = grad_patch[0]
             params_grad_patch = grad_patch[1:]
@@ -203,8 +180,9 @@ class _overlap_tile_function(torch.autograd.Function):
                         
             # Trim padded input grad to remove padding.
             input_grad = input_grad_padded[indices]
+        #print(input_grad.std().data[0], [p.std().data[0] for p in params_grad])
         
-        return (input_grad,) + (None,)*7 + tuple(params_grad)
+        return (input_grad,) + (None,)*6 + tuple(params_grad)
 
 
 class overlap_tile(torch.nn.Module):
@@ -217,31 +195,28 @@ class overlap_tile(torch.nn.Module):
             tiles to take from the input. The output tile size is determined
             automatically.
         model (Module) : A pytorch module implementing the model/layer to
-            do overlap-tile with.
+            do overlap-tile with. Convolutions must have no zero-padding,
+            must have stride of 1, and no spatial pooling; the output after
+            overlap-tile is assumed to have the same spatial size as the input.
         in_channels (int) : The number of input channels.
         out_channels (int) : The number of output channels.
-        output_size (tuple of int OR function) : The complete output size of
-            the model. If left as `None`, the output size is assumed to be the
-            same as the input size, but with `out_channels` channels.
-            Alternatively, a function can be passed that computes the output
-            size given an input size. The function must take only the spatial
-            size of the input and return the spatial size of the output,
-            leaving out the batch and channel dimensions.
         use_gpu (bool) : Whether to use the GPU for memory and compute.
         device (int) : If using the GPU, which device to use.
+        verbose (bool) : Whether to print the autodetermined output patch
+            size.
     """
     def __init__(self, input_patch_size, model, in_channels, out_channels,
-                 output_size=None, use_gpu=False, device=None):
+                 use_gpu=False, device=None, verbose=True):
         super(overlap_tile, self).__init__()
         self.input_patch_size = input_patch_size
         self.model = model
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.output_size = output_size
         self.output_patch_size = None
         self.use_gpu = use_gpu
         self.device = device
         self._modules['model'] = model
+        self.verbose = verbose
         
     def cuda(self, device=None):
         self.use_gpu = True
@@ -265,6 +240,10 @@ class overlap_tile(torch.nn.Module):
                 _in = _to_cuda(_in, self.device)
             _out = self.model(_in)
             self.output_patch_size = tuple(_out.size()[2:])
+            if self.verbose:
+                print("OVERLAP-TILE: For input patch size {}, output patch "
+                      "size is {}."
+                      "".format(self.input_patch_size, self.output_patch_size))
             
         # Forward pass.
         output = _overlap_tile_function.apply(input,
@@ -272,7 +251,6 @@ class overlap_tile(torch.nn.Module):
                                               self.input_patch_size,
                                               self.output_patch_size,
                                               self.out_channels,
-                                              self.output_size,
                                               self.use_gpu,
                                               self.device,
                                               *self.model.parameters())
@@ -283,21 +261,23 @@ class overlap_tile(torch.nn.Module):
 if __name__=='__main__':
     class identity_module(torch.nn.Module):
         def forward(self, input):
-            return input+2
+            return input
 
     model = identity_module()
-    #model = torch.nn.Conv2d(1, 1, 3)
-    tiler = overlap_tile(input_patch_size=(3,5), model=model)
+    #model = torch.nn.Conv2d(1, 1, 3, padding=0)
+    tiler = overlap_tile(input_patch_size=(3,5), model=model,
+                         in_channels=1, out_channels=1)
 
     edge = 10
     im_input = np.reshape(range(edge**2), (1,1,edge,edge)).astype(np.float32)
     im_input = Variable(torch.from_numpy(im_input))
     im_output = tiler(im_input)
 
-    if np.all(im_output==(im_input+2)):
+    if np.all(im_output==(im_input)):
         print("Test PASSED")
     else:
         print("Test FAILED")
+    print(im_input.size(), im_output.size())
         
     #loss = im_output.mean()-1
     #loss.backward()

architectures/revnet.py
@@ -7,7 +7,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-from fcn_maker.blocks import convolution
+from fcn_maker.blocks import (convolution,
+                              max_pooling)
 from .blocks import (batch_normalization,
                      rev_block,
                      reversible_basic_block)
@@ -191,10 +192,13 @@ class dilated_fcn(base_model):
         self.layers_down = []
         self.layers_across = []
         self.layers_up = []
+        padding = 1
+        if overlap_tile_patch_size is not None:
+            padding = 0
         preprocessor = convolution(in_channels=in_channels,
                                    out_channels=filters[0],
                                    kernel_size=3,
-                                   padding=1,
+                                   padding=padding,
                                    init=init,
                                    ndim=ndim)
         if overlap_tile_patch_size is not None:
@@ -212,18 +216,15 @@ class dilated_fcn(base_model):
                                     out_channels=filters[i+1],
                                     kernel_size=3,
                                     padding=padding,
-                                    stride=2,
                                     init=init,
                                     ndim=ndim)
             if overlap_tile_patch_size is not None:
-                def output_size(input_size):
-                    return tuple([s//2 for s in input_size])
                 conv_down = overlap_tile(overlap_tile_patch_size,
                                          model=conv_down,
                                          in_channels=conv_down.in_channels,
-                                         out_channels=conv_down.out_channels,
-                                         output_size=output_size)
+                                         out_channels=conv_down.out_channels)
             prev_channels = filters[i+1]
+            self.layers_down.append(max_pooling(kernel_size=2, ndim=ndim))
             self.layers_down.append(conv_down)
         for i in range(num_blocks):
             block = block_type(in_channels=prev_channels,
@@ -253,11 +254,11 @@ class dilated_fcn(base_model):
                                        model=conv_up,
                                        in_channels=conv_up.in_channels,
                                        out_channels=conv_up.out_channels)
-                # TODO: Memorize the output_size on the downscaling path
-                #       and crop outputs to that size since they may become
-                #       larger with some input sizes.
+                ## TODO: Memorize the output_size on the downscaling path
+                ##       and crop outputs to that size since they may become
+                ##       larger with some input sizes.
             prev_channels = filters_i
-            self.layers_up.append(torch.nn.Upsample(scale_factor=2))
+            self.layers_up.append(torch.nn.Upsample(scale_factor=ndim))
             self.layers_up.append(conv_up)
         self.classifier = None
         if classes is not None: