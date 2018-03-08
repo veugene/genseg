200330dcd68cf32a91aa9588a4c6d41712d41dff
Eugene Vorontsov <veugene@users.noreply.github.com>
2018-03-08 11:49:31 -0500

    Brats (#4)
    Add code for preparing BRATS data and for loading and preprocessing it.

---
+++ scripts/prepare_brats_data.py
@@ -0,0 +1,231 @@
+import os
+import argparse
+from collections import OrderedDict
+#from concurrent.futures import ThreadPoolExecutor
+import threading
+try:
+    import queue            # python 3
+except ImportError:
+    import Queue as queue   # python 2
+
+import numpy as np
+from scipy import ndimage
+import SimpleITK as sitk
+import zarr
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="Prepare BRATS data. Loads "
+        "BRATS 2017 data and stores volume slices in a zarr archive. "
+        "Slices are organized as a group per patient, containing three "
+        "groups: \'sick\', \'healthy\', and \'segmentations\'. Sick cases "
+        "contain any anomolous class, healthy cases contain no anomalies, and "
+        "segmentations are the segmentations of the anomalies. Each group "
+        "contains subgroups for each of the three orthogonal planes along "
+        "which slices are extracted. For each case, MRI sequences are stored "
+        "in a single volume, indexed along the first axis in the following "
+        "order: flair, t1ce, t1, t2. All slices are cropped to the minimal "
+        "bounding box containing the brain.")
+    parser.add_argument('--data_dir',
+                        help="The directory containing the BRATS 2017 data. "
+                             "Either HGG or LGG.",
+                        required=True, type=str)
+    parser.add_argument('--save_to',
+                        help="Path to save the zarr archive to.",
+                        required=True, type=str)
+    parser.add_argument('--skip_bias_correction',
+                        help="Whether to skip N4 bias correction.",
+                        required=False, action='store_true')
+    parser.add_argument('--num_threads',
+                        help="The number of parallel threads to execute.",
+                        required=False, type=int, default=None)
+    return parser.parse_args()
+
+
+def data_loader(data_dir):
+    tags = ['flair', 't1ce', 't1', 't2', 'seg']
+    for dn in sorted(os.listdir(data_dir)):
+        path = os.path.join(data_dir, dn)
+        
+        # Match filenames to tags.
+        # NOTE: t1ce chosen when both t1 and t1ce matched
+        fn_dict = OrderedDict()
+        for fn in sorted(os.listdir(path)):
+            match = [t for t in tags if t in fn]
+            fn_dict[match[0]] = fn
+            
+        # Load files.
+        vol_all = []
+        segmentation = None
+        size = None
+        for t in tags:
+            vol = sitk.ReadImage(os.path.join(path, fn_dict[t]))
+            vol_np = sitk.GetArrayFromImage(vol)
+            
+            if size is None:
+                size = vol_np.shape
+            if vol_np.shape != size:
+                raise Exception("Expected {} to have a size of {} but got {}."
+                                "".format(fn_dict[t], size, vol_np.shape))
+            
+            if t=='seg':
+                segmentation = vol_np.astype(np.int64)
+                segmentation = np.expand_dims(segmentation, 0)
+            else:
+                vol_np = vol_np.astype(np.float32)
+                vol_all.append(np.expand_dims(vol_np, 0))
+        
+        # Concatenate on channel axis.
+        volume = np.concatenate(vol_all, axis=0)
+        
+        # Crop to volume.
+        bbox = ndimage.find_objects(volume!=0)[0]
+        volume = volume[bbox]
+        segmentation = segmentation[bbox]
+            
+        yield volume, segmentation, dn
+        
+        
+def get_slices(volume, segmentation, axis):
+    # Axis transpose order.
+    order = [axis]+[i for i in range(4) if i!=axis]
+    
+    # Fill this dict.
+    slices_dict = OrderedDict()
+    
+    # Everything without anomalies is healthy.
+    indices = np.where(segmentation==0)
+    indices_slice = [slice(None, None)]*4
+    indices_slice[axis] = np.unique(indices[axis])
+    slices_dict['healthy'] = volume[indices_slice].transpose(order)
+    
+    # Locate anomalies. These are sick slices.
+    indices = np.where(segmentation)
+    indices_slice = [slice(None, None)]*4
+    indices_slice[axis] = np.unique(indices[axis])
+    slices_dict['sick'] = volume[indices_slice].transpose(order)
+    slices_dict['segmentation'] = segmentation[indices_slice].transpose(order)
+        
+    return slices_dict
+
+
+def preprocess(volume, segmentation, skip_bias_correction=False):
+    volume_out = volume.copy()
+    corrector = sitk.N4BiasFieldCorrectionImageFilter()
+    corrector.SetMaximumNumberOfIterations([50,50,50,10])
+    seg_sitk = sitk.GetImageFromArray(segmentation[0])
+    for i, vol in enumerate(volume):
+        # N4 bias correction.
+        if not skip_bias_correction:
+            vol_sitk = sitk.GetImageFromArray(vol)
+            out_sitk = corrector.Execute(vol_sitk, seg_sitk)
+            out = sitk.GetArrayFromImage(out_sitk)
+        else:
+            out = vol.copy()
+        
+        # Mean center and normalize by std.
+        out_nonzero_view = out[out>0]
+        out -= out_nonzero_view.mean()
+        out /= out_nonzero_view.std()
+        
+        volume_out[i] = out
+        
+    return volume_out
+
+
+def process_case(case_num, zarr_storage, volume, segmentation, fn,
+                 skip_bias_correction=False):
+    print("Processing case {}: {}".format(case_num, fn))
+    group_p = zarr_storage.create_group(str(case_num))
+    # TODO: set attribute containing fn.
+    for axis in [1,2,3]:
+        volume = preprocess(volume, segmentation, skip_bias_correction)
+        slices = get_slices(volume, segmentation, axis=axis)
+        for key in slices.keys():
+            group_k = group_p.require_group(key)
+            compressor = zarr.Blosc(cname='lz4', clevel=5, shuffle=1)
+            group_k.create_dataset('axis_{}'.format(axis),
+                                   shape=slices[key].shape,
+                                   data=slices[key],
+                                   dtype=slices[key].dtype,
+                                   chunks=slices[key].shape[1:],
+                                   compressor=compressor)
+                                       
+                                       
+class thread_pool_executor(object):
+    def __init__(self, max_workers, block=False):
+        self.max_workers = max_workers
+        if not block:
+            self._task_queue = queue.Queue()
+        else:
+            self._task_queue = queue.Queue(max_workers)
+        self._run_thread = None
+        
+    def submit(self, func, *args, **kwargs):
+        self._task_queue.put([func, args, kwargs])
+        
+        if self._run_thread is None:
+            self._run_thread = threading.Thread(\
+                target=thread_pool_executor._run,
+                args=(self._task_queue, self.max_workers)
+            )
+            self._run_thread.daemon = True
+            self._run_thread.start()
+        
+    @staticmethod
+    def _run(task_queue, max_workers):
+        threads = {}
+        termination_queue = queue.Queue()
+        for i in range(max_workers):
+            termination_queue.put(i)
+            
+        def do_task(func, args, kwargs, thread_num, termination_queue):
+            func(*args, **kwargs)
+            termination_queue.put(thread_num)
+            
+        while 1:
+            task = task_queue.get()
+            if task is None:
+                for i in threads:
+                    threads[i].join()
+                break
+            func, args, kwargs = task
+            thread_num = termination_queue.get()
+            if thread_num in threads:
+                threads[thread_num].join()
+            threads[thread_num] = threading.Thread(\
+                target=do_task,
+                args=(func, args, kwargs, thread_num, termination_queue)
+            )
+            threads[thread_num].daemon = True
+            threads[thread_num].start()
+            
+    def shutdown(self, wait=False):
+        if not wait:
+            try:
+                while 1:
+                    self._task_queue.get()
+            except queue.Empty:
+                pass
+        self._task_queue.put(None)
+        if wait:
+            self._run_thread.join()
+
+
+if __name__=='__main__':
+    args = parse()
+    #if os.path.exists(args.save_to):
+        #raise ValueError("Path to save data already exists. Aborting.")
+    zarr_storage = zarr.open_group(store=args.save_to, mode='w', path="/")
+        
+    try:
+        num_threads = args.num_threads
+        if num_threads is None:
+            num_threads = os.cpu_count()
+        executor = thread_pool_executor(max_workers=num_threads, block=True)
+        for i, (vol, seg, fn) in enumerate(data_loader(args.data_dir)):
+            executor.submit(process_case, i, zarr_storage, vol, seg, fn,
+                            args.skip_bias_correction)
+        executor.shutdown(wait=True)
+    except KeyboardInterrupt:
+        pass
--- utils/data.py
+++ utils/data.py
@@ -1,12 +1,17 @@
 from __future__ import (print_function,
                         division)
 import os
+from collections import OrderedDict
+
+import zarr
 import imageio
 import numpy as np
 from skimage import transform
+
 from data_tools.wrap import (delayed_view,
                              multi_source_array)
 from data_tools.io import data_flow
+from data_tools.data_augmentation import image_stack_random_transform
 
 
 def resize_stack(arr, size, interp='bilinear'):
@@ -76,3 +81,182 @@ class data_flow_sampler(data_flow):
             if reshuffle:
                 self.data[i].arr.re_shuffle()
         return super(data_flow_sampler, self).flow()
+
+
+def prepare_data_brats(path_hgg, path_lgg):
+    """
+    Convenience function to prepare brats data as multi_source_array objects,
+    split into training and validation subsets.
+    
+    path_hgg (string) : Path of the zarr directory containing the HGG data.
+    path_lgg (string) : Path of the zarr directory containing the LGG data.
+    
+    Returns six arrays: healthy slices, sick slices, and segmentations for 
+    the training and validation subsets.
+    """
+    
+    # Random 20% data split.
+    validation_indices = {'hgg': [60,54,182,64,166,190,184,143,6,75,169,183,
+                                  202,166,189,41,158,69,133,180,16,41,0,198,
+                                  101,120,5,185,203,151,100,149,44,48,151,34,
+                                  88,204,149,119,152,65],
+                          'lgg': [25,14,25,4,54,56,56,54,59,1,38,6,24,23,53]}
+    orientations = [1,2,3]
+    
+    
+    def _prepare(path, axis, validation_indices):
+        # Open zarr file.
+        try:
+            zgroup = zarr.open_group(path, mode='r')
+        except:
+            print("Failed to open data: {}".format(path))
+            raise
+        
+        # Assemble volumes and corresponding segmentations.
+        volumes_h = []
+        volumes_s = []
+        segmentations = []
+        for key in zgroup.keys():   # Per patient.
+            group_p = zgroup[key]
+            volumes_h.append(group_p['healthy/axis_{}'.format(str(axis))])
+            volumes_s.append(group_p['sick/axis_{}'.format(str(axis))])
+            segmentations.append(\
+                        group_p['segmentation/axis_{}'.format(str(axis))])
+        
+        # Split data into training and validation.
+        training_indices = [i for i in range(len(segmentations)) \
+                            if i not in validation_indices]
+        h_train = [volumes_h[i] for i in training_indices]
+        h_valid = [volumes_h[i] for i in validation_indices]
+        s_train = [volumes_s[i] for i in training_indices]
+        s_valid = [volumes_s[i] for i in validation_indices]
+        m_train = [segmentations[i] for i in training_indices]
+        m_valid = [segmentations[i] for i in validation_indices]
+        
+        return h_train, h_valid, s_train, s_valid, m_train, m_valid
+    
+    def _extend(target, source):
+        for i in range(len(source)):
+            if len(target) < len(source):
+                target.append(source[i])
+            else:
+                target[i].extend(source[i])
+        return target
+    
+    data_hgg = []
+    data_lgg = []
+    for axis in orientations:
+        _extend(data_hgg, _prepare(path_hgg, axis, validation_indices['hgg']))
+        _extend(data_lgg, _prepare(path_lgg, axis, validation_indices['lgg']))
+    
+    data = OrderedDict([('train', OrderedDict()),
+                        ('valid', OrderedDict())])
+    msa = multi_source_array
+    data['train']['h'] = msa(data_hgg[0]+data_lgg[0], no_shape=True)
+    data['valid']['h'] = msa(data_hgg[1]+data_lgg[1], no_shape=True)
+    data['train']['s'] = msa(data_hgg[2]+data_lgg[2], no_shape=True)
+    data['valid']['s'] = msa(data_hgg[3]+data_lgg[3], no_shape=True)
+    data['train']['m'] = msa(data_hgg[4]+data_lgg[4], no_shape=True)
+    data['valid']['m'] = msa(data_hgg[5]+data_lgg[5], no_shape=True)
+        
+    return data
+
+
+def preprocessor_brats(data_augmentation_kwargs=None):
+    """
+    Preprocessor function to pass to a data_flow, for BRATS data.
+    """
+    
+    def f(batch):
+        # Expecting two or three sources in the batch, with np.float32 dtype
+        # unless there is a segmentation in which case it is expected to have
+        # type np.int64.
+        assert len(batch) in [2, 3]
+        has_segmentations = False
+        if len(batch)==2:
+            assert batch[0][0].dtype==np.float32
+            assert batch[1][0].dtype==np.float32 or batch[1][0].dtype==np.int64
+            if batch[1][0].dtype==np.int64:
+                has_segmentations = True
+        if len(batch)==3:
+            assert batch[0][0].dtype==np.float32
+            assert batch[1][1].dtype==np.float32
+            assert batch[2][0].dtype==np.float32 or batch[2][0].dtype==np.int64
+            if batch[2][0].dtype==np.int64:
+                has_segmentations = True
+        
+        # Center all slices onto empty buffers with a size equal to the
+        # largest.
+        out_batch = []
+        for b in batch:
+            # Find the largest slice.
+            max_shape = (0,0)
+            for im in b:
+                im_shape = np.shape(im)[1:]
+                max_shape = (max(im_shape[0], max_shape[0]),
+                             max(im_shape[1], max_shape[1]))
+            
+            # Create buffer and center images into it.
+            out_b = np.zeros((len(b),len(b[0]))+max_shape, dtype=np.float32)
+            out_b[...] = np.inf     # To be replaced with background intensity.
+            for i, im in enumerate(b):
+                im_shape = np.shape(im)[1:]
+                offset = np.subtract(max_shape, im_shape)//2
+                index_slices = [slice(None, None),
+                                slice(offset[0], offset[0]+im_shape[0]),
+                                slice(offset[1], offset[1]+im_shape[1])]
+                out_b[i][index_slices] = im[...]
+            out_batch.append(out_b)
+        
+        # Data augmentation.
+        if data_augmentation_kwargs is not None:
+                
+            # Segmentation setup : sick, segmentations
+            if len(batch)==2 and has_segmentations:
+                out_batch = image_stack_random_transform(out_batch[0],
+                                                         y=out_batch[1],
+                                                    **data_augmentation_kwargs)
+            
+            # Translation setup : healthy, sick, (segmentations)
+            else:
+                b0 = out_batch[0]
+                b1 = out_batch[1]
+                b2 = None
+                if len(out_batch)==3:
+                    b2 = out_batch[2]
+                    
+                b0 = image_stack_random_transform(b0,
+                                                  **data_augmentation_kwargs)
+                b_ = image_stack_random_transform(b1, y=b2,
+                                                  **data_augmentation_kwargs)
+                if b2 is None:
+                    b1 = b_
+                    out_batch = (b0, b1)
+                else:
+                    b1, b2 = b_
+                    out_batch = (b0, b1, b2)
+                        
+        # Set background intensity in output buffers.
+        for b, out_b in zip(batch, out_batch):
+            for i, im in enumerate(b):
+                # HACK: Assuming corner pixel is always outside of the brain.
+                background = im[:,0,0]
+                for j in range(len(im)):
+                    im_buff = out_b[i,j]
+                    im_buff[im_buff==np.inf] = background[j]
+                    
+        # Remove distant outlier intensities.
+        out_batch = list(out_batch)
+        out_batch[0] = np.clip(out_batch[0], -2.0, 2.0)
+        if len(batch)==3:
+            out_batch[1] = np.clip(out_batch[1], -2.0, 2.0)
+        if not has_segmentations:
+            out_batch[-1] = np.clip(out_batch[-1], -2.0, 2.0)
+        
+        # Set dtype.
+        n = len(batch)
+        out_batch = [out_batch[i].astype(batch[i][0].dtype) for i in range(n)]
+                        
+        return out_batch
+    return f
+        
