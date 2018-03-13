2645aae923d615e03cdd3d53d67705d8ae1ca2c0
Christopher Beckham <chrispy645@gmail.com>
2018-03-12 21:18:01 -0400

    - Update code to work on (semi-) latest version of Ignite. - Model resuming implemented, where `--resume=xx` will load the model, its weights, and the optim state. - Architecture is no longer hardcoded, instead uses `--model_from=xx` where `xx` is an (arg-less) method in `configs.py`. - Add option to turn cuda mode off. - Add image saver code from Eugene so that visualisations from valid set are dumped to experiments folder.

--- brats_segmentation.py
+++ brats_segmentation.py
@@ -6,6 +6,7 @@ import os
 import shutil
 import argparse
 from datetime import datetime
+import warnings
 
 import numpy as np
 from scipy.misc import imsave
@@ -13,19 +14,21 @@ import torch
 from torch.autograd import Variable
 import ignite
 from ignite.trainer import Trainer
-from ignite.evaluator import Evaluator
 from ignite.engine import Events
-from ignite.handlers import Evaluate
+from ignite.evaluator import Evaluator
+from ignite.handlers import ModelCheckpoint
 
 from architectures.image2image import DilatedFCN
 from utils.ignite import (progress_report,
-                          metrics_handler)
+                          metrics_handler,
+                          image_saver)
 from utils.metrics import (dice_loss,
                            accuracy)
 from utils.data import data_flow_sampler
 from utils.data import prepare_data_brats
 from utils.data import preprocessor_brats
 from util import count_params
+import configs
 from fcn_maker.model import assemble_resunet
 from fcn_maker.blocks import (tiny_block,
                               basic_block)
@@ -35,47 +38,26 @@ Process arguments.
 '''
 def parse_args():
     parser = argparse.ArgumentParser(description="Segmentation on BRATS 2017.")
-    parser.add_argument('--arch', type=str, default='resunet')
+    parser.add_argument('--name', type=str, default=None)
     parser.add_argument('--data_dir', type=str, default='/home/eugene/data/')
     parser.add_argument('--save_path', type=str, default='./experiments')
     g_load = parser.add_mutually_exclusive_group(required=False)
-    g_load.add_argument('--model_from', type=str, default=None)
-    g_load.add_argument('--load', type=str, default=None)
+    g_load.add_argument('--model_from', type=str, default='resunet')
+    g_load.add_argument('--resume', type=str, default=None)
     parser.add_argument('-e', '--evaluate', action='store_true')
     parser.add_argument('--weight_decay', type=float, default=1e-4)
     parser.add_argument('--batch_size', type=int, default=80)
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--learning_rate', type=float, default=0.001)
     parser.add_argument('--optimizer', type=str, default='RMSprop')
+    parser.add_argument('--cuda', type=bool, default=True)
     args = parser.parse_args()
     return args
 
-'''
-Settings.
-'''
-model_kwargs_dilated = {'in_channels': 4, 'C': 24, 'classes': 3}
-model_kwargs = OrderedDict((
-    ('in_channels', 4),
-    ('num_classes', 4),
-    ('num_init_blocks', 2),
-    ('num_main_blocks', 3),
-    ('main_block_depth', 1),
-    ('init_num_filters', 32),
-    ('dropout', 0.),
-    ('main_block', basic_block),
-    ('init_block', tiny_block),
-    ('norm_kwargs', {'momentum': 0.1}),
-    ('nonlinearity', 'ReLU'),
-    ('ndim', 2)
-))
-
-
+  
 if __name__ == '__main__':
     args = parse_args()
-    assert args.arch in ['vanilla_dilated_fcn', 'resunet']
-    if args.arch=='vanilla_dilated_fcn':
-        model_kwargs = model_kwargs_dilated
-    
+
     '''
     Prepare data -- load, standardize, add channel dim., shuffle, split.
     '''
@@ -109,32 +91,52 @@ if __name__ == '__main__':
                                      nb_proc_workers=0,
                                      rng=np.random.RandomState(42))
 
-    '''
-    Prepare model.
-    '''
-    if args.model_from is not None:
-        raise NotImplemented("Loading model with \'--model_from\' not "
-                             "implemented.")
-    elif args.load is not None:
-        raise NotImplemented("Loading model with \'--load\' not implemented.")
-    else:
-        torch.backends.cudnn.benchmark = False   # profiler
-        if args.arch == 'vanilla_dilated_fcn':
-            model = DilatedFCN(**model_kwargs)
-            print(model)
-        else:
-            model = assemble_resunet(**model_kwargs)
-        print("Number of parameters: {}".format(count_params(model)))
-        model.cuda()
-        if args.optimizer=='RMSprop':
+    def get_optimizer(name, model, lr):
+        if name == 'RMSprop':
             optimizer = torch.optim.RMSprop(params=model.parameters(),
-                                            lr=args.learning_rate,
+                                            lr=lr,
                                             alpha=0.9,
                                             weight_decay=args.weight_decay)
+            return optimizer
         else:
             raise NotImplemented("Optimizer {} not supported."
                                 "".format(args.optimizer))
 
+    '''
+    Prepare model. The `resume` arg is able to restore the model,
+    its state, and the optimizer's state, whereas `model_from`
+    (which is mutually exclusive with `resume`) only loads the
+    desired architecture.
+    '''
+    torch.backends.cudnn.benchmark = False   # profiler
+    if args.resume is not None:
+        saved_dict = torch.load(args.resume)
+        model = getattr(configs, saved_dict['model_from'])()
+        if args.cuda:
+            model.cuda()
+        model.load_state_dict(saved_dict['weights'])
+        optimizer = get_optimizer(args.optimizer, model, args.learning_rate)
+        #import pdb
+        #pdb.set_trace()
+        optimizer.load_state_dict(saved_dict['optim'])
+    else:
+        model = getattr(configs, args.model_from)()
+        if args.cuda:
+            model.cuda()
+        optimizer = get_optimizer(args.optimizer, model, args.learning_rate)
+    print("Number of parameters: {}".format(count_params(model)))
+
+    '''
+    Set up experiment directory.
+    '''
+    name = "brats_seg" if args.name is None else args.name
+    exp_id = "%s_{0:%Y-%m-%d}_{0:%H-%M-%S}".format(datetime.now()) % name
+    path = os.path.join(args.save_path, exp_id)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(os.path.join(path, "cmd.sh"), 'w') as f:
+        f.write(' '.join(sys.argv))
+        
     '''
     Set up loss functions and metrics. Since this is a multiclass problem,
     set up a metrics handler for each output map.
@@ -143,18 +145,32 @@ if __name__ == '__main__':
     loss_functions = []
     metrics_dict = OrderedDict()
     for idx,l in enumerate(labels):
-        dice = dice_loss(l,idx).cuda()
+        dice = dice_loss(l,idx)
+        if args.cuda:
+            dice = dice.cuda()
         loss_functions.append(dice)
         metrics_dict['dice{}'.format(l)] = dice
     metrics = metrics_handler(metrics_dict)
-
+    
+    '''
+    Visualize validation outputs.
+    '''
+    bs = args.batch_size
+    epoch_length = lambda ds : len(ds)//bs + int(len(ds)%bs>0)
+    num_batches_valid = epoch_length(data['valid']['s'])
+    image_saver_valid = image_saver(save_path=os.path.join(path, "validation"),
+                                    epoch_length=num_batches_valid)
+    
     '''
     Set up training and evaluation functions.
     '''
     def prepare_batch(batch):
         b0, b1 = batch
-        b0 = Variable(torch.from_numpy(np.array(b0))).cuda()
-        b1 = Variable(torch.from_numpy(np.array(b1))).cuda()
+        b0 = Variable(torch.from_numpy(np.array(b0)))
+        b1 = Variable(torch.from_numpy(np.array(b1)))
+        if args.cuda:
+            b0 = b0.cuda()
+            b1 = b1.cuda()
         return b0, b1
 
     def training_function(batch):
@@ -179,42 +195,37 @@ if __name__ == '__main__':
             loss = 0.
             for i in range(len(loss_functions)):
                 loss += loss_functions[i](output, batch[1])
+            image_saver_valid(batch[0], batch[1], output)
             return loss.data.item(), metrics(output, batch[1])
     evaluator = Evaluator(validation_function)
-
-    '''
-    Set up experiment directory.
-    '''
-    exp_id = "brats_seg_{0:%Y-%m-%d}_{0:%H-%M-%S}".format(datetime.now())
-    path = os.path.join(args.save_path, exp_id)
-    if not os.path.exists(path):
-        os.makedirs(path)
-    with open(os.path.join(path, "cmd.sh"), 'w') as f:
-        f.write(' '.join(sys.argv))
-    if args.model_from is not None:
-        model_fn = os.path.basename(args.model_from)
-        shutil.copyfile(args.model_from, os.path.join(path, model_fn))
-
+    
     '''
     Set up logging to screen.
     '''
     bs = args.batch_size
-    epoch_length = lambda ds : len(ds)//bs + int(len(ds)%bs>0)
-    num_batches_train = epoch_length(data['train']['s'])
-    num_batches_valid = epoch_length(data['valid']['s'])
-    progress_train = progress_report(epoch_length=num_batches_train,
+    progress_train = progress_report(prefix="tr",
                                      log_path=os.path.join(path,
                                                            "log_train.txt"))
-    progress_valid = progress_report(epoch_length=num_batches_valid,
-                                     prefix="val",
-                                     progress_bar=True,
+    progress_valid = progress_report(prefix="val",
                                      log_path=os.path.join(path,
                                                            "log_valid.txt"))
     trainer.add_event_handler(Events.ITERATION_COMPLETED, progress_train)
+    cpt_handler = ModelCheckpoint(dirname=path,
+                                  filename_prefix='weights',
+                                  save_interval=1,
+                                  n_saved=5,
+                                  atomic=False,
+                                  exist_ok=True,
+                                  create_dir=True)
     trainer.add_event_handler(Events.EPOCH_COMPLETED,
-                              Evaluate(evaluator,
-                                       loader_valid,
-                                       epoch_interval=1))
+                              cpt_handler,
+                              {'model':
+                               {'weights': model.state_dict(),
+                                'model_from': args.model_from,
+                                'optim': optimizer.state_dict()}
+                              })
+    trainer.add_event_handler(Events.EPOCH_COMPLETED,
+                              lambda engine, state: evaluator.run(loader_valid))
     evaluator.add_event_handler(Events.ITERATION_COMPLETED, progress_valid)
 
     '''
--- utils/ignite.py
+++ utils/ignite.py
@@ -1,50 +1,53 @@
 from __future__ import print_function
+import os
+import numpy as np
 from collections import OrderedDict
+from scipy.misc import imsave
 from tqdm import tqdm
 
 class progress_report(object):
-    def __init__(self, epoch_length, prefix=None, progress_bar=True,
+    def __init__(self, prefix=None, progress_bar=True,
                  log_path=None):
-        self.epoch_length = epoch_length
         self.prefix = prefix
         self.progress_bar = progress_bar
         self.log_path = log_path
         self._pbar = None
-        
-    def __call__(self, engine):
+        self.metrics = None
+
+    def __call__(self, engine, state):
         prefix = ""
         if self.prefix is not None:
             prefix = "{}_".format(self.prefix)
+        if (state.iteration-1) % len(state.dataloader) == 0:
+            # Every time we're at the start of the next epoch, reset
+            # the statistics.
+            self.metrics = OrderedDict(((prefix+'loss', 0),))
+        iter_history = state.output # only this iteration's history
+        loss = iter_history[0]
+        self.metrics[prefix+'loss'] += loss
+        for name, val in iter_history[-1].items():
+            name = prefix+name
+            if name not in self.metrics:
+                self.metrics[name] = 0
+            self.metrics[name] += val
         # Average metrics over the epoch thus far.
-        metrics = OrderedDict(((prefix+'loss', 0),))
-        iteration_in_epoch = (engine.current_iteration-1)%self.epoch_length+1
-        idx_epoch_start = engine.current_iteration - iteration_in_epoch
-        for iter_history in engine.history[idx_epoch_start:]:
-            loss = iter_history[0]
-            metrics[prefix+'loss'] += loss
-            for name, val in iter_history[-1].items():
-                name = prefix+name
-                if name not in metrics:
-                    metrics[name] = 0
-                metrics[name] += val
-        for name in metrics:
-            metrics[name] /= float(iteration_in_epoch)
-                                    
-        
+        metrics = OrderedDict()
+        for name in self.metrics:
+            metrics[name] = self.metrics[name] / float(state.iteration)
         # Print to screen.
         def log_string(desc, metrics, file=None):
             mstr = " ".join(["{}={:.3}".format(*x) for x in metrics.items()])
             desc = "{}: {}".format(desc, mstr) if len(desc) else mstr
             print(desc, file=file)
         desc = ""
-        if hasattr(engine, 'current_epoch'):
-            desc += "Epoch {}".format(engine.current_epoch)
+        if hasattr(state, 'epoch'):
+            desc += "Epoch {}".format(state.epoch)
         if self.progress_bar:
             if self._pbar is None:
-                self._pbar = tqdm(total=self.epoch_length, desc=desc)
+                self._pbar = tqdm(total=len(state.dataloader), desc=desc)
             self._pbar.set_postfix(metrics)
             self._pbar.update(1)
-            if (engine.current_iteration)%self.epoch_length==0:
+            if state.iteration % len(state.dataloader) == 0:
                 self._pbar.close()
                 self._pbar = None
                 if self.log_path is not None:
@@ -52,19 +55,82 @@ class progress_report(object):
                         log_string(desc, metrics, file=logfile)
         else:
             log_string(desc, metrics)
-            
-            
+
+
 class metrics_handler(object):
     def __init__(self, measure_functions_dict=None):
         self.measure_functions = measure_functions_dict
         if measure_functions_dict is None:
             self.measure_functions = OrderedDict()
-            
+
     def __call__(self, output, target):
         measures = OrderedDict()
         for m in self.measure_functions:
             measures[m] = self.measure_functions[m](output, target).data.item()
         return measures
-        
+
     def add(self, metric_name, function):
-        self.measure_functions[metric_name] = function 
+        self.measure_functions[metric_name] = function
+
+'''
+Save images on validation.
+'''
+class image_saver(object):
+    def __init__(self, save_path, epoch_length):
+        self.save_path = save_path
+        self.epoch_length = epoch_length
+        self._current_batch_num = 0
+        self._current_epoch = 0
+
+    def __call__(self, inputs, target, prediction):
+        # Current batch size.
+        this_batch_size = len(target)
+
+        # Current batch_num, epoch.
+        self._current_batch_num += 1
+        if self._current_batch_num==self.epoch_length:
+            self._current_epoch += 1
+            self._current_batch_num = 0
+
+        # Make directory.
+        save_dir = os.path.join(self.save_path, str(self._current_epoch))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        # Variables to numpy.
+        inputs = inputs.cpu().numpy()
+        target = target.cpu().numpy()
+        prediction = prediction.detach().cpu().numpy()
+
+        # Visualize.
+        for i in range(this_batch_size):
+
+            # inputs
+            im_i = []
+            for x in inputs[i]:
+                im_i.append(self._process_slice((x+2)/4.))
+
+            # target
+            im_t = [self._process_slice(target[i]/4.)]
+
+            # prediction
+            p = prediction[i]
+            p[0] = 0
+            p[1] *= 1
+            p[2] *= 2
+            p[3] *= 4
+            p = p.max(axis=0)
+            im_p = [self._process_slice(p/4.)]
+
+            out_image = np.concatenate(im_i+im_t+im_p, axis=1)
+            imsave(os.path.join(save_dir,
+                                "{}_{}.png"
+                                "".format(self._current_batch_num, i)),
+                   out_image)
+
+    def _process_slice(self, s):
+        s = np.squeeze(s)
+        s = np.clip(s, 0, 1)
+        s[0,0]=1
+        s[0,1]=0
+        return s
