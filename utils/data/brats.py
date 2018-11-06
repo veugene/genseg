veugene/genseg

commit b3b81f3a9dfe2f9f6081cb2f8030e59c29ad07b2

Adjust entire pipeline to allow slice-conditional translation (BRATS).

utils/data/brats.py
@@ -124,25 +124,36 @@ def _prepare_data_brats(path_hgg, path_lgg, validation_indices,
         # Assemble volumes and corresponding segmentations.
         volumes_h = []
         volumes_s = []
-        segmentations = []
+        volumes_m = []
+        indices_h = []
+        indices_s = []
         for _key in h5py_file.keys():   # Per patient.
             group_p = h5py_file[_key]
             volumes_h.append(group_p['healthy/axis_{}'.format(str(axis))])
             volumes_s.append(group_p['sick/axis_{}'.format(str(axis))])
-            segmentations.append(\
-                        group_p['segmentation/axis_{}'.format(str(axis))])
+            volumes_m.append(group_p['segmentation/axis_{}'.format(str(axis))])
+            indices_h.append(group_p['h_indices/axis_{}'.format(str(axis))])
+            indices_s.append(group_p['s_indices/axis_{}'.format(str(axis))])
         
         # Split data into training and validation.
         h_train = [volumes_h[i] for i in training_indices[key]]
         h_valid = [volumes_h[i] for i in validation_indices[key]]
         s_train = [volumes_s[i] for i in training_indices[key]]
         s_valid = [volumes_s[i] for i in validation_indices[key]]
-        m_train = [segmentations[i] if i not in masked_indices[key]
-                   else np.array([None]*len(segmentations[i]))
+        m_train = [volumes_m[i] if i not in masked_indices[key]
+                   else np.array([None]*len(volumes_m[i]))
                    for i in training_indices[key]]
-        m_valid = [segmentations[i] for i in validation_indices[key]]
+        m_valid = [volumes_m[i] for i in validation_indices[key]]
+        hi_train = [indices_h[i] for i in training_indices[key]]
+        hi_valid = [indices_h[i] for i in validation_indices[key]]
+        si_train = [indices_s[i] for i in training_indices[key]]
+        si_valid = [indices_s[i] for i in validation_indices[key]]
         
-        return h_train, h_valid, s_train, s_valid, m_train, m_valid
+        return (h_train,  h_valid,
+                s_train,  s_valid,
+                m_train,  m_valid,
+                hi_train, hi_valid,
+                si_train, si_valid)
     
     def _extend(target, source):
         for i in range(len(source)):
@@ -161,12 +172,16 @@ def _prepare_data_brats(path_hgg, path_lgg, validation_indices,
     data = OrderedDict([('train', OrderedDict()),
                         ('valid', OrderedDict())])
     msa = multi_source_array
-    train_h = data_hgg[0]+data_lgg[0]
-    valid_h = data_hgg[1]+data_lgg[1]
-    train_s = data_hgg[2]+data_lgg[2]
-    valid_s = data_hgg[3]+data_lgg[3]
-    train_m = data_hgg[4]+data_lgg[4]
-    valid_m = data_hgg[5]+data_lgg[5]
+    train_h  = data_hgg[0]+data_lgg[0]
+    valid_h  = data_hgg[1]+data_lgg[1]
+    train_s  = data_hgg[2]+data_lgg[2]
+    valid_s  = data_hgg[3]+data_lgg[3]
+    train_m  = data_hgg[4]+data_lgg[4]
+    valid_m  = data_hgg[5]+data_lgg[5]
+    train_hi = data_hgg[6]+data_lgg[6]
+    valid_hi = data_hgg[7]+data_lgg[7]
+    train_si = data_hgg[8]+data_lgg[8]
+    valid_si = data_hgg[9]+data_lgg[9]
 
     # HACK: we may have a situation where the number of sick examples
     # is greater than the number of healthy. In that case, we should
@@ -177,30 +192,30 @@ def _prepare_data_brats(path_hgg, path_lgg, validation_indices,
     len_s = sum([ len(elem) for elem in train_s])
     if len_h < len_s:
         m = int(np.ceil(len_s / len_h))
-    data['train']['h'] = msa(train_h*m, no_shape=True)
-    data['valid']['h'] = msa(valid_h*m, no_shape=True)
-    data['train']['s'] = msa(train_s, no_shape=True)
-    data['valid']['s'] = msa(valid_s, no_shape=True)
-    data['train']['m'] = msa(train_m, no_shape=True)
-    data['valid']['m'] = msa(valid_m, no_shape=True)
+    data['train']['h']  = msa(train_h*m, no_shape=True)
+    data['valid']['h']  = msa(valid_h*m, no_shape=True)
+    data['train']['s']  = msa(train_s, no_shape=True)
+    data['valid']['s']  = msa(valid_s, no_shape=True)
+    data['train']['m']  = msa(train_m, no_shape=True)
+    data['valid']['m']  = msa(valid_m, no_shape=True)
+    data['train']['hi'] = msa(train_hi, no_shape=True)
+    data['valid']['hi'] = msa(valid_hi, no_shape=True)
+    data['train']['si'] = msa(train_si, no_shape=True)
+    data['valid']['si'] = msa(valid_si, no_shape=True)
         
     return data
 
 
-def preprocessor_brats(data_augmentation_kwargs=None,
-                       h_idx=0, s_idx=1, m_idx=2):
+def preprocessor_brats(data_augmentation_kwargs=None):
     """
     Preprocessor function to pass to a data_flow, for BRATS data.
     
     data_augmentation_kwargs : Dictionary of keyword arguments to pass to
         the data augmentation code (image_stack_random_transform).
-    h_idx : The batch index for the healthy slices (None if not in batch).
-    s_idx : The batch index for the sick slices (None if not in batch).
-    m_idx : The batch index for the segmentation masks (None if not in batch).
     """
         
-    def process_element(h, s, m, max_shape):
-        inputs = [h, s, m]
+    def process_element(inputs, max_shape):
+        h, s, m, hi, si = inputs
         
         # Center slices onto empty buffers with a size equal to the largest.
         elem = []
@@ -234,7 +249,7 @@ def preprocessor_brats(data_augmentation_kwargs=None,
                 s = _
                         
         # Set background intensity.
-        for im, im_orig in zip([h, s, m], inputs):
+        for im, im_orig in zip([h, s, m], inputs[:-2]):
             if im is None:
                 continue
             # HACK: Assuming corner pixel is always outside of the brain.
@@ -253,19 +268,22 @@ def preprocessor_brats(data_augmentation_kwargs=None,
         
         # Set dtype (all output buffers are float32 to support inf).
         elem = []
-        for im, im_orig in zip([h, s, m], inputs):
+        for im, im_orig in zip([h, s, m], inputs[:-2]):
             if im is None:
                 elem.append(None)
                 continue
             im = im.astype(im_orig.dtype)
             elem.append(im)
+        
+        # Append indices.
+        elem.extend(inputs[-2:])
             
         return elem
         
     def process_batch(batch):        
         # Find the largest slice.
         max_shape = (0,0)
-        for b in batch:
+        for b in batch[:-2]:
             for im in b:
                 if im is None:
                     continue
@@ -276,17 +294,9 @@ def preprocessor_brats(data_augmentation_kwargs=None,
         # Process every element.
         elements = []
         for i in range(len(batch[0])):
-            h = None if h_idx is None else batch[h_idx][i]
-            s = None if s_idx is None else batch[s_idx][i]
-            m = None if m_idx is None else batch[m_idx][i]
-            elem = process_element(h=h, s=s, m=m, max_shape=max_shape)
+            elem = process_element([b[i] for b in batch], max_shape=max_shape)
             elements.append(elem)
         out_batch = list(zip(*elements))
-        
-        # Drop batch indices that are passed as `None`.
-        out_batch = [out_batch[i] \
-                     for i, elem_idx in enumerate([h_idx, s_idx, m_idx]) \
-                     if elem_idx is not None]
         
         return out_batch
     

brats_segmentation.py
@@ -47,6 +47,7 @@ def get_parser():
                        choices=['brats13s', 'brats17'])
     g_exp.add_argument('--data_dir', type=str, default='./data/brats/2013')
     g_exp.add_argument('--orientation', type=int, default=None)
+    g_exp.add_argument('--slice_conditional', action='store_true')
     g_exp.add_argument('--save_path', type=str, default='./experiments')
     mutex_from = g_exp.add_mutually_exclusive_group()
     mutex_from.add_argument('--model_from', type=str, default=None)
@@ -199,8 +200,10 @@ def run():
                               masked_fraction=1.-args.labeled_fraction,
                               drop_masked=args.yield_only_labeled,
                               rng=np.random.RandomState(args.rseed))
-    data_train = [data['train']['h'], data['train']['s'], data['train']['m']]
-    data_valid = [data['valid']['h'], data['valid']['s'], data['valid']['m']]
+    data_train = [data['train']['h'], data['train']['s'], data['train']['m'],
+                  data['train']['hi'], data['train']['si']]
+    data_valid = [data['valid']['h'], data['valid']['s'], data['valid']['m'],
+                  data['valid']['hi'], data['valid']['si']]
     loader = {
         'train': data_flow_sampler(data_train,
                                    sample_random=True,
@@ -220,12 +223,15 @@ def run():
                                    rng=np.random.RandomState(args.rseed))}
     
     # Function to convert data to pytorch usable form.
-    def prepare_batch(batch):
-        h, s, m = batch
+    def prepare_batch(batch, slice_conditional=False):
+        h, s, m, h_indices, s_indices = batch
         # Prepare for pytorch.
         h = Variable(torch.from_numpy(np.array(h))).cuda()
         s = Variable(torch.from_numpy(np.array(s))).cuda()
-        return h, s, m
+        # Provide slice index tuple if slice_conditional.
+        if not slice_conditional:
+            h_indices = s_indices = None
+        return h, s, m, h_indices, s_indices
     
     # Helper for training/validation loops : detach variables from graph.
     def detach(x):
@@ -239,9 +245,10 @@ def run():
     def training_function(engine, batch):
         for model in experiment_state.model.values():
             model.train()
-        B, A, M = prepare_batch(batch)
+        B, A, M, I_A, I_B = prepare_batch(batch, args.slice_conditional)
         outputs = experiment_state.model['G'](A, B, M,
-                                         optimizer=experiment_state.optimizer)
+                                         optimizer=experiment_state.optimizer,
+                                         class_A=I_A, class_B=I_B)
         outputs = detach(outputs)
         
         # Drop images without labels, for visualization.
@@ -258,9 +265,10 @@ def run():
     def validation_function(engine, batch):
         for model in experiment_state.model.values():
             model.eval()
-        B, A, M = prepare_batch(batch)
+        B, A, M, I_A, I_B = prepare_batch(batch, args.slice_conditional)
         with torch.no_grad():
-            outputs = experiment_state.model['G'](A, B, M, rng=engine.rng)
+            outputs = experiment_state.model['G'](A, B, M, rng=engine.rng,
+                                                  class_A=I_A, class_B=I_B)
         outputs = detach(outputs)
         return outputs
     

model/bd_segmentation.py
@@ -110,14 +110,17 @@ class segmentation_model(nn.Module):
         if torch.cuda.device_count()>1:
             self._loss_G = nn.DataParallel(self._loss_G, output_device=-1)
         
-    def forward(self, x_A, x_B, mask=None, optimizer=None, disc=None,
-                rng=None):
+    def forward(self, x_A, x_B, mask=None, class_A=None, class_B=None,
+                optimizer=None, disc=None, rng=None):
         # Compute gradients and update?
         do_updates_bool = True if optimizer is not None else False
         
         # Compute all outputs.
         with torch.set_grad_enabled(do_updates_bool):
-            visible, hidden, intermediates = self._forward(x_A, x_B, rng=rng)
+            visible, hidden, intermediates = self._forward(x_A, x_B,
+                                                           class_A=class_A,
+                                                           class_B=class_B,
+                                                           rng=rng)
         
         # Evaluate discriminator loss and update.
         loss_disc = defaultdict(int)
@@ -128,6 +131,8 @@ class segmentation_model(nn.Module):
                 with torch.set_grad_enabled(do_updates_bool):
                     loss_disc, loss_mi_est = self._loss_D(x_A=x_A,
                                                           x_B=x_B,
+                                                          class_A=class_A,
+                                                          class_B=class_B,
                                                           x_BA=visible['x_BA'],
                                                           x_AB=visible['x_AB'],
                                                           c_A=hidden['c_A'],
@@ -161,9 +166,11 @@ class segmentation_model(nn.Module):
         with torch.set_grad_enabled(do_updates_bool):
             losses_G = self._loss_G(x_AM=visible['x_AM'],
                                     x_A=x_A,
+                                    class_A=class_A,
                                     x_AB=visible['x_AB'],
                                     x_AA=visible['x_AA'],
                                     x_B=x_B,
+                                    class_B=class_B,
                                     x_BA=visible['x_BA'],
                                     x_BB=visible['x_BB'],
                                     x_BAB=visible['x_BAB'],
@@ -249,7 +256,7 @@ class _forward(nn.Module):
         ret = ret.to(torch.cuda.current_device())
         return ret
     
-    def forward(self, x_A, x_B, rng=None):
+    def forward(self, x_A, x_B, class_A=None, class_B=None, rng=None):
         assert len(x_A)==len(x_B)
         batch_size = len(x_A)
         
@@ -280,14 +287,16 @@ class _forward(nn.Module):
         
         # A->(B, dA)->A
         x_AB = x_AB_residual = X_AA = x_AA_list = c_A = u_A = None
+        info_AB = {'skip_info': skip_A}
+        if class_A is not None:
+            info_AB['class_info'] = class_A
         if (self.lambda_seg
          or self.lambda_disc or self.lambda_x_id or self.lambda_z_id):
-            x_AB_residual, skip_AM = self.decoder_residual(s_A,
-                                                           skip_info=skip_A)
+            x_AB_residual, skip_AM = self.decoder_residual(s_A, **info_AB)
         if self.lambda_disc or self.lambda_x_id or self.lambda_z_id:
             c_A, u_A = torch.split(s_A, [s_A.size(1)-self.shape_sample[0],
                                          self.shape_sample[0]], dim=1)
-            x_AB, _ = self.decoder_common(c_A, skip_info=skip_A)
+            x_AB, _ = self.decoder_common(c_A, **info_AB)
             x_AA = add(x_AB, x_AB_residual)
             
             # Unpack.
@@ -297,12 +306,15 @@ class _forward(nn.Module):
         
         # B->(B, dA)->A
         x_BA = x_BA_residual = x_BB = z_BA = x_BB_list = None
+        info_BA = {'skip_info': skip_B}
+        if class_A is not None:
+            info_BA['class_info'] = class_B
         if self.lambda_disc or self.lambda_x_id or self.lambda_z_id:
             u_BA = self._z_sample(batch_size, rng=rng)
             c_B  = s_B[:,:s_B.size(1)-self.shape_sample[0]]
             z_BA = torch.cat([c_B, u_BA], dim=1)
-            x_BB, _ = self.decoder_common(c_B, skip_info=skip_B)
-            x_BA_residual, _ = self.decoder_residual(z_BA, skip_info=skip_B)
+            x_BB, _ = self.decoder_common(c_B, **info_BA)
+            x_BA_residual, _ = self.decoder_residual(z_BA, **info_BA)
             x_BA = add(x_BB, x_BA_residual)
             
             # Unpack.
@@ -317,7 +329,10 @@ class _forward(nn.Module):
                 x_AM = self.segmenter[0](s_A, skip_info=skip_AM)
             else:
                 # Re-use residual decoder in mode 1.
-                x_AM = self.decoder_residual(s_A, skip_info=skip_AM, mode=1)
+                info_AM = {'skip_info': skip_AM}
+                if class_A is not None:
+                    info_AM['class_info'] = class_A
+                x_AM = self.decoder_residual(s_A, **info_AM, mode=1)
                 x_AM, _ = unpack(x_AM)
         
         # Reconstruct latent codes.
@@ -332,10 +347,13 @@ class _forward(nn.Module):
         
         # Cycle.
         x_BAB = c_BA = u_BA = None
+        info_BAB = {'skip_info': skip_BA}
+        if class_A is not None:
+            info_BAB['class_info'] = class_B
         if self.lambda_cyc:
             c_BA, u_BA = torch.split(s_BA, [s_BA.size(1)-self.shape_sample[0],
                                             self.shape_sample[0]], dim=1)
-            x_BAB, _ = self.decoder_common(c_BA, skip_info=skip_BA)
+            x_BAB, _ = self.decoder_common(c_BA, **info_BAB)
             x_BAB, _ = unpack(x_BAB)
         
         # Compile outputs and return.
@@ -390,7 +408,8 @@ class _loss_D(nn.Module):
                     'disc_B'    : disc_B,
                     'mi'        : mi_estimator}  # Separate params.
     
-    def forward(self, x_A, x_B, x_BA, x_AB, c_A, u_A, c_BA, u_BA):
+    def forward(self, x_A, x_B, x_BA, x_AB, c_A, u_A, c_BA, u_BA,
+                class_A=None, class_B=None):
         # If outputs are lists, get the last item (image).
         if not isinstance(x_BA, torch.Tensor):
             x_BA = x_BA[-1]
@@ -398,11 +417,17 @@ class _loss_D(nn.Module):
             x_AB = x_AB[-1]
         
         # Discriminators.
+        kwargs_real = None if class_A is None else {'class_info': class_A}
+        kwargs_fake = None if class_B is None else {'class_info': class_B}
         loss_disc = OrderedDict()
         loss_disc_A = self._gan.D(self.net['disc_A'],
-                                  fake=x_BA.detach(), real=x_A)
+                                  fake=x_BA.detach(), real=x_A,
+                                  kwargs_real=kwargs_real,
+                                  kwargs_fake=kwargs_fake)
         loss_disc_B = self._gan.D(self.net['disc_B'],
-                                  fake=x_AB.detach(), real=x_B)
+                                  fake=x_AB.detach(), real=x_B,
+                                  kwargs_real=kwargs_real,
+                                  kwargs_fake=kwargs_fake)
         loss_disc['A'] = self.lambda_disc*loss_disc_A
         loss_disc['B'] = self.lambda_disc*loss_disc_B
         
@@ -437,7 +462,8 @@ class _loss_G(nn.Module):
     
     def forward(self, x_AM, x_A, x_AB, x_AA, x_B, x_BA, x_BB, x_BAB,
                 s_BA, s_AA, c_AB, c_BB, z_BA, s_A, c_A, u_A, c_B, c_BA, u_BA,
-                x_AA_list, x_BB_list, skip_A, skip_B):
+                x_AA_list, x_BB_list, skip_A, skip_B,
+                class_A=None, class_B=None):
         # Mutual information loss for generator.
         loss_mi_gen = defaultdict(int)
         if self.net['mi'] is not None:
@@ -445,12 +471,20 @@ class _loss_G(nn.Module):
             loss_mi_gen['BA'] = -self.lambda_mi*self.net['mi'](c_BA, u_BA)
         
         # Generator loss.
+        kwargs_real = None if class_A is None else {'class_info': class_A}
+        kwargs_fake = None if class_B is None else {'class_info': class_B}
         loss_gen = defaultdict(int)
         if self.lambda_disc:
-            loss_gen['AB'] = self.lambda_disc*self._gan.G(self.net['disc_B'],
-                                                          fake=x_AB, real=x_B)
-            loss_gen['BA'] = self.lambda_disc*self._gan.G(self.net['disc_A'],
-                                                          fake=x_BA, real=x_A)
+            loss_gen['AB'] = self.lambda_disc*self._gan.G(
+                                    self.net['disc_B'],
+                                    fake=x_AB, real=x_B,
+                                    kwargs_real=kwargs_real,
+                                    kwargs_fake=kwargs_fake)
+            loss_gen['BA'] = self.lambda_disc*self._gan.G(
+                                    self.net['disc_A'],
+                                    fake=x_BA, real=x_A,
+                                    kwargs_real=kwargs_real,
+                                    kwargs_fake=kwargs_fake)
         
         # Reconstruction loss.
         loss_rec = defaultdict(int)

model/common/losses.py
@@ -65,27 +65,39 @@ class gan_objective(object):
         else:
             raise ValueError("Unknown objective: {}".format(objective))
     
-    def G(self, disc, fake, real=None):
+    def G(self, disc, fake, real=None, kwargs_fake=None, kwargs_real=None):
+        if kwargs_fake is None: kwargs_fake = {}
+        if kwargs_real is None: kwargs_real = {}
         if self.relativistic:
             return self._foreach(lambda x: self._G(x[0]-x[1]),
-                                 [disc(fake), disc(real)])
-        return self._foreach(self._G, disc(fake))
+                                 [disc(fake, **kwargs_fake),
+                                  disc(real, **kwargs_real)])
+        return self._foreach(self._G, disc(fake, **kwargs_fake))
     
-    def D(self, disc, fake, real):
+    def D(self, disc, fake, real, kwargs_fake=None, kwargs_real=None):
+        if kwargs_fake is None: kwargs_fake = {}
+        if kwargs_real is None: kwargs_real = {}
         if self.relativistic:
             loss_real = self._D_relativistic(real, fake, disc, self._D1,
                                              self.grad_penalty_real,
-                                             self.grad_penalty_fake)
+                                             self.grad_penalty_fake,
+                                             kwargs_a=kwargs_real,
+                                             kwargs_b=kwargs_fake)
             loss_fake = self._D_relativistic(fake, real, disc, self._D0,
                                              self.grad_penalty_fake,
-                                             self.grad_penalty_real)
+                                             self.grad_penalty_real,
+                                             kwargs_a=kwargs_fake,
+                                             kwargs_b=kwargs_real)
         else:
-            loss_real = self._D(real, disc, self._D1, self.grad_penalty_real)
-            loss_fake = self._D(fake, disc, self._D0, self.grad_penalty_fake)
+            loss_real = self._D(real, disc, self._D1, self.grad_penalty_real,
+                                kwargs_disc=kwargs_real)
+            loss_fake = self._D(fake, disc, self._D0, self.grad_penalty_fake,
+                                kwargs_disc=kwargs_fake)
         return loss_real+loss_fake
     
-    def _D(self, x, disc, objective, grad_penalty):
-        disc_out = disc(x)
+    def _D(self, x, disc, objective, grad_penalty, kwargs_disc=None):
+        if kwargs_disc is None: kwargs_disc = {}
+        disc_out = disc(x, **kwargs_disc)
         loss = self._foreach(objective, disc_out)
         if grad_penalty is not None:
             if isinstance(disc_out, torch.Tensor):
@@ -105,12 +117,15 @@ class gan_objective(object):
         return loss
     
     def _D_relativistic(self, a, b, disc, objective,
-                        grad_penalty_a, grad_penalty_b):
+                        grad_penalty_a, grad_penalty_b,
+                        kwargs_a=None, kwargs_b=None):
+        if kwargs_a is None: kwargs_a = {}
+        if kwargs_b is None: kwargs_b = {}
         if torch.is_grad_enabled():
             a.requires_grad = True
             b.requires_grad = True
-        disc_a = disc(a)
-        disc_b = disc(b)
+        disc_a = disc(a, **kwargs_a)
+        disc_b = disc(b, **kwargs_b)
         loss = self._foreach(lambda x: objective(x[0]-x[1]), [disc_a, disc_b])
         if torch.is_grad_enabled():
             if grad_penalty_a is not None or grad_penalty_b is not None:

model/common/network/basic.py
@@ -951,18 +951,20 @@ def grad_norm(module):
     return norm
 
 
-def recursive_spectral_norm(module):
+def recursive_spectral_norm(module, types=None):
     """
     Recursively traverse submodules in a module and apply spectral norm to
     all convolutional layers.
     """
+    if types is None:
+        types = tuple()
     for m in module.modules():
         if isinstance(m, (nn.Conv1d,
                           nn.Conv2d,
                           nn.Conv3d,
                           nn.ConvTranspose1d,
                           nn.ConvTranspose2d,
-                          nn.ConvTranspose3d)):
+                          nn.ConvTranspose3d)+types):
             if not hasattr(m, '_has_spectral_norm'):
                 spectral_norm(m)
             setattr(m, '_has_spectral_norm', True)

scripts/data_preparation/prepare_brats_data_hemispheres.py
@@ -99,7 +99,7 @@ def data_loader(data_dir, crop=True):
         yield volume, segmentation, dn
         
         
-def get_slices(volume, segmentation, brain_mask,
+def get_slices(volume, segmentation, brain_mask, indices,
                min_tumor_fraction, min_brain_fraction):
     
     assert min_tumor_fraction>=0 and min_tumor_fraction<=1
@@ -133,6 +133,8 @@ def get_slices(volume, segmentation, brain_mask,
     slices_dict['healthy'] = volume[indices_healthy]
     slices_dict['sick'] = volume[indices_anomaly]
     slices_dict['segmentation'] = segmentation[indices_anomaly]
+    slices_dict['h_indices'] = indices[indices_healthy]
+    slices_dict['s_indices'] = indices[indices_anomaly]
     
     return slices_dict
 
@@ -156,6 +158,16 @@ def preprocess(volume, segmentation, skip_bias_correction=False):
     brain_mask = volume_out!=0
     volume_out[brain_mask] -= volume_out[brain_mask].mean()
     volume_out[brain_mask] /= volume_out[brain_mask].std()*5
+    
+    # Get slice indices, with 0 at the center.
+    #brain_mask_ax1 = brain_mask.sum(axis=(0,2,3))>0
+    #idx_min = np.argmax(brain_mask_ax1)
+    #idx_max = len(brain_mask_ax1)-1-np.argmax(np.flipud(brain_mask_ax1))
+    #idx_mid = (idx_max-idx_min)//2
+    #a = idx_mid-len(brain_mask_ax1)
+    #b = len(brain_mask_ax1)+a-1
+    #indices = np.arange(a, b)
+    indices = np.arange(brain_mask.shape[1])
         
     # Split volume along hemispheres.
     mid0 = volume.shape[-1]//2
@@ -168,8 +180,9 @@ def preprocess(volume, segmentation, skip_bias_correction=False):
                                        segmentation[:,:,:,mid1:]], axis=1)
     brain_mask = np.concatenate([brain_mask[:,:,:,:mid0],
                                  brain_mask[:,:,:,mid1:]], axis=1)
+    indices = np.concatenate([indices, indices])
     
-    return volume_out, segmentation_out, brain_mask
+    return volume_out, segmentation_out, brain_mask, indices
 
 
 def process_case(case_num, h5py_file, volume, segmentation, fn,
@@ -178,8 +191,10 @@ def process_case(case_num, h5py_file, volume, segmentation, fn,
     print("Processing case {}: {}".format(case_num, fn))
     group_p = h5py_file.create_group(str(case_num))
     # TODO: set attribute containing fn.
-    vol, seg, m = preprocess(volume, segmentation, skip_bias_correction)
-    slices = get_slices(vol, seg, m, min_tumor_fraction, min_brain_fraction)
+    vol, seg, m, indices = preprocess(volume, segmentation,
+                                      skip_bias_correction)
+    slices = get_slices(vol, seg, m, indices,
+                        min_tumor_fraction, min_brain_fraction)
     for key in slices.keys():
         if len(slices[key])==0:
             kwargs = {}

commit 71e607ed1b2dc13f2443d5a9a5e7910cbbc57f63

Fix error introduced in b3b81f3a9dfe2f9f6081cb2f8030e59c29ad07b2 (indices not multiplied, unlike healthy slices).

utils/data/brats.py
@@ -192,16 +192,16 @@ def _prepare_data_brats(path_hgg, path_lgg, validation_indices,
     len_s = sum([ len(elem) for elem in train_s])
     if len_h < len_s:
         m = int(np.ceil(len_s / len_h))
-    data['train']['h']  = msa(train_h*m, no_shape=True)
-    data['valid']['h']  = msa(valid_h*m, no_shape=True)
-    data['train']['s']  = msa(train_s, no_shape=True)
-    data['valid']['s']  = msa(valid_s, no_shape=True)
-    data['train']['m']  = msa(train_m, no_shape=True)
-    data['valid']['m']  = msa(valid_m, no_shape=True)
-    data['train']['hi'] = msa(train_hi, no_shape=True)
-    data['valid']['hi'] = msa(valid_hi, no_shape=True)
-    data['train']['si'] = msa(train_si, no_shape=True)
-    data['valid']['si'] = msa(valid_si, no_shape=True)
+    data['train']['h']  = msa(train_h*m,  no_shape=True)
+    data['valid']['h']  = msa(valid_h*m,  no_shape=True)
+    data['train']['s']  = msa(train_s,    no_shape=True)
+    data['valid']['s']  = msa(valid_s,    no_shape=True)
+    data['train']['m']  = msa(train_m,    no_shape=True)
+    data['valid']['m']  = msa(valid_m,    no_shape=True)
+    data['train']['hi'] = msa(train_hi*m, no_shape=True)
+    data['valid']['hi'] = msa(valid_hi*m, no_shape=True)
+    data['train']['si'] = msa(train_si,   no_shape=True)
+    data['valid']['si'] = msa(valid_si,   no_shape=True)
         
     return data
 