Fix use of sigmoid instead of softmax in multi-class brats segmentation.

model/configs/brats_2017/ae_seg/ae_003.py
@@ -68,7 +68,8 @@ def build_model():
                                       output_transform=torch.tanh,
                                       long_skip_merge_mode=None),   # pool
         'decoder_seg'       : decoder(**decoder_kwargs,
-                                      output_transform=torch.sigmoid,
+                                      output_transform=lambda x: \
+                                                torch.softmax(x, dim=1),
                                       long_skip_merge_mode='skinny_cat',
                                       num_classes=1)}
     

model/configs/brats_2017/ae_seg/ae_003_mc_seg_only.py
@@ -66,7 +66,8 @@ def build_model():
         'encoder'           : encoder_instance,
         'decoder_rec'       : None,
         'decoder_seg'       : decoder(**decoder_kwargs,
-                                      output_transform=torch.sigmoid,
+                                      output_transform=lambda x: \
+                                                torch.softmax(x, dim=1),
                                       long_skip_merge_mode='skinny_cat',
                                       num_classes=4)}
     

model/configs/brats_2017/bds3/bds3_018_pre.py
@@ -457,7 +457,10 @@ class decoder(nn.Module):
             return out, (skip_info, adain_params)
         elif mode==1:
             out = self.classifier(out)
-            out = torch.sigmoid(out)
+            if self.num_classes==1:
+                out = torch.sigmoid(out)
+            else:
+                out = torch.softmax(out, dim=1)
             return out
         else:
             AssertionError()