1efd02a085cae48a9b3045d1c028537c19f93d0f
Eugene Vorontsov <evorontsov@nvidia.com>
2018-11-30 13:01:24 -0800

    Debugged/rewrote ddsm data loading.

--- utils/data/ddsm.py
+++ utils/data/ddsm.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 import h5py
 import numpy as np
+from skimage import filters
 from skimage import transform
 
 from data_tools.wrap import multi_source_array
@@ -46,21 +47,23 @@ def prepare_data_ddsm(path, masked_fraction=0, drop_masked=False, rng=None):
     except:
         print("Failed to open data: {}".format(path))
         raise
-    case_id_s_set = set(h5py_file['sick'].keys())
-    case_id_h_set = set(h5py_file['healthy'].keys())
-    case_id_i_set = cases_s.intersection(cases_h)
-    case_id_h_unique = [case in h5py_file['healthy'].keys()
-                        if case in case_id_h_set.difference(case_id_i_set)]
+    def get(keys, indices): # Index into keys object.
+        return [k for i, k in enumerate(keys) if i in indices]
     rnd_state = np.random.RandomState(0)
-    indices_s = rnd_state.permutation(len(case_id_s_set))
-    indices_h = rnd_state.permutation(len(case_id_h_unique))
+    indices_s = rnd_state.permutation(len(h5py_file['sick'].keys()))
+    indices_h = rnd_state.permutation(len(h5py_file['healthy'].keys()))
     n_valid = int(0.2*len(indices_s))   # Determined by sick cases.
-    assert n_valid > len(case_id_h_set)
-    indices_s_train, indices_s_valid = np.split(indices_s, [n_valid])
-    case_id_s['valid'] = h5py_file['sick'].keys()[indices_sick_valid]
-    case_id_s['train'] = h5py_file['sick'].keys()[indices_sick_train]
-    case_id_h['valid'] = ( list(case_id_i_set)
-                          +case_id_h_unique[indices_h[:n_valid]])
+    indices_s_valid, indices_s_train = np.split(indices_s, [n_valid])
+    case_id_s['valid'] = get(h5py_file['sick'].keys(), indices_s_valid)
+    case_id_s['train'] = get(h5py_file['sick'].keys(), indices_s_train)
+    
+    case_id_h_and_s = list(filter(lambda x:x in case_id_s['valid'],
+                                  h5py_file['healthy'].keys()))
+    n_random = n_valid-len(case_id_h_and_s)
+    case_id_only_h = get(h5py_file['healthy'].keys(), indices_h[:n_random])
+    case_id_h['valid'] = case_id_h_and_s+case_id_only_h
+    case_id_h['train'] = list(filter(lambda x:x not in case_id_h['valid'],
+                                     h5py_file['healthy'].keys()))
     
     # Assemble cases with corresponding segmentations; split train/valid.
     cases_h = {'train': [], 'valid': []}
@@ -69,11 +72,15 @@ def prepare_data_ddsm(path, masked_fraction=0, drop_masked=False, rng=None):
     for split in ['train', 'valid']:
         for case_id in case_id_s[split]:
             f = h5py_file['sick'][case_id]
-            for key in filter(lambda x:x.startswith('abnormality'), f.keys()):
-                cases_s[split].append(f[key]['cropped_image'])
-                cases_m[split].append(f[key]['cropped_mask'])
+            for view in f.keys():
+                for key in filter(lambda x:x.startswith('abnormality'),
+                                  f[view].keys()):
+                    cases_s[split].append(f[view][key]['cropped_image'])
+                    cases_m[split].append(f[view][key]['cropped_mask'])
         for case_id in case_id_h[split]:
-            cases_h[split].append(h5py_file['healthy'][case_id]['patch'])
+            f = h5py_file['healthy'][case_id]
+            for view in f.keys():
+                cases_h[split].append(f[view]['patch'])
 
     # Cases with these indices will either be dropped from the training
     # set or have their segmentations set to None.
@@ -83,7 +90,11 @@ def prepare_data_ddsm(path, masked_fraction=0, drop_masked=False, rng=None):
     num_cases_total  = len(cases_m['train'])
     num_cases_masked = int(min(num_cases_total,
                                num_cases_total*masked_fraction+0.5))
+    if masked_fraction < 1: # At least 1 unmasked.
+        num_cases_masked = max(num_cases_total-1, num_cases_masked)
     masked_indices = rng.permutation(num_cases_total)[:num_cases_masked]
+    print("DEBUG: A total of {}/{} images are labeled."
+          "".format(num_cases_total-num_cases_masked, num_cases_total))
     
     # Apply masking in one of two ways.
     # 
@@ -93,22 +104,18 @@ def prepare_data_ddsm(path, masked_fraction=0, drop_masked=False, rng=None):
     # OR if `drop_masked` is True:
     # 
     # 2. Remove all cases indexed with `masked_indices`.
-    cases_h_train = []
     cases_s_train = []
     cases_m_train = []
     for i in range(num_cases_total):
-        if drop_masked:
-            # Drop.
-            continue
-        elif i not in masked_indices:
+        if i in masked_indices:
+            # Mask out or drop.
+            if drop_masked:
+                continue    # Drop.
+            cases_m_train.append(None)
+        else:
             # Keep.
             cases_m_train.append(cases_m['train'][i])
-        else:
-            # Mask out.
-            cases_m_train.append(np.array([None]))
-        cases_h_train.append(cases_h['train'][i])
         cases_s_train.append(cases_s['train'][i])
-    cases_h['train'] = cases_h_train
     cases_s['train'] = cases_s_train
     cases_m['train'] = cases_m_train
     
@@ -125,10 +132,23 @@ def prepare_data_ddsm(path, masked_fraction=0, drop_masked=False, rng=None):
         len_s = sum([len(elem) for elem in cases_s[key]])
         if len_h < len_s:
             m = int(np.ceil(len_s / len_h))
-        data[key]['h']  = multi_source_array(cases_h[key]*m, no_shape=True)
-        data[key]['s']  = multi_source_array(cases_s[key],   no_shape=True)
-        data[key]['m']  = multi_source_array(cases_m[key],   no_shape=True)
+        data[key]['h'] = _list(cases_h[key]*m, np.uint16)
+        data[key]['s'] = _list(cases_s[key], np.uint16)
+        data[key]['m'] = _list(cases_m[key], np.uint8)
     return data
+
+
+class _list(object):
+    def __init__(self, indexable, dtype):
+        self._items = indexable
+        self.dtype = dtype
+    def __getitem__(self, idx):
+        elem = self._items[idx]
+        if elem is not None:
+            elem = elem[...]
+        return elem
+    def __len__(self):
+        return len(self._items)
 
 
 def preprocessor_ddsm(output_size, data_augmentation_kwargs=None):
@@ -139,31 +159,59 @@ def preprocessor_ddsm(output_size, data_augmentation_kwargs=None):
     data_augmentation_kwargs : Dictionary of keyword arguments to pass to
         the data augmentation code (image_stack_random_transform).
     """
-    def process_batch(batch):
-        h, s, m = batch
-        h, s, m = resize(h), resize(s), resize(m, interp='constant')
+    def process_element(inputs):
+        h, s, m = inputs
+        # Float, rescale, center.
+        h = h.astype(np.float32)
+        s = s.astype(np.float32)
+        h = h/2**15 - 1
+        s = s/2**15 - 1
+        
+        # Resize.
+        h = resize(h, size=(output_size, output_size), order=1)
+        s = resize(s, size=(output_size, output_size), order=1)
+        if m is not None:
+            m = resize(m, size=(output_size, output_size), order=0)
+        
+        # Expand dims.
+        h = np.expand_dims(h, 0)
+        s = np.expand_dims(s, 0)
+        if m is not None:
+            m = np.expand_dims(m, 0)
+        
+        # Data augmentation.
         if data_augmentation_kwargs is not None:
-            h    = image_random_transform(h, **data_augmentation_kwargs,
-                                          n_warp_threads=1)
-            s, m = image_random_transform(s, m, **data_augmentation_kwargs,
-                                          n_warp_threads=1)
+            h = image_random_transform(h, **data_augmentation_kwargs,
+                                       n_warp_threads=1)
+            _ = image_random_transform(s, m, **data_augmentation_kwargs,
+                                       n_warp_threads=1)
+            if m is not None:
+                s, m = _
+        
         return h, s, m
+    
+    def process_batch(batch):
+        elements = []
+        for i in range(len(batch[0])):
+            elem = process_element([b[i] for b in batch])
+            elements.append(elem)
+        out_batch = list(zip(*elements))
+        return out_batch
+    
     return process_batch
 
 
-def resize(image_list, size, interp='bilinear'):
-    """
-    Resize each image in a list.
-    """
-    out = []
-    for image in image_list:
-        out_image = np.zeros(image.shape[:-2]+size, dtype=image.dtype)
-        for idx in np.ndindex(image.shape[:-2]):
-            out_image[idx] = transform.resize(image[idx],
-                                              output_shape=size,
-                                              mode=interp,
-                                              cval=0,
-                                              clip=False,
-                                              preserve_range=True)
-        out.append(out_image)
-    return out
+def resize(image, size, order=False):
+    out_image = np.zeros(image.shape[:-2]+size, dtype=image.dtype)
+    for idx in np.ndindex(image.shape[:-2]):
+        if any([a<b for a,b in zip(size, image.shape[-2:])]):
+            # Downscaling - smooth, first.
+            s = [(1-float(a)/b)/2. for a,b in zip(size, image.shape[-2:])]
+            image[idx] = filters.gaussian(image[idx], sigma=s)
+        out_image[idx] = transform.resize(image[idx],
+                                          output_shape=size,
+                                          mode='constant',
+                                          cval=0,
+                                          clip=False,
+                                          preserve_range=True)
+    return out_image
