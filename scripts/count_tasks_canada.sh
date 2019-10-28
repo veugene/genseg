ea457a4e6fad30a0128d9834875d84acc8b657d6
Eugene Vorontsov <veugene@beluga1.int.ets1.calculquebec.ca>
2019-10-28 18:26:51 -0400

    Output all experiments when counting.

--- scripts/count_tasks_canada.sh
+++ scripts/count_tasks_canada.sh
@@ -2,4 +2,6 @@
 TMP_SACCT_OUT=`sacct -u veugene`
 N_PENDING=$(grep PENDING <<< "$TMP_SACCT_OUT" |wc -l)
 N_RUNNING=$(expr $(grep RUNNING <<< "$TMP_SACCT_OUT" |wc -l) / 2)
+echo "$TMP_SACCT_OUT"
+echo ""
 echo "PENDING: $N_PENDING  RUNNING: $N_RUNNING"
