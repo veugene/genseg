dfdaac7cfbd287834f119b4a30e0bd253aec1b60
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2020-09-08 06:09:13 -0400

    Change DDSM data preparation to convert all images from raw DDSM and heuristically match CBIS-DDSM masks to these images.

--- scripts/data_preparation/prepare_ddsm_data.py
+++ scripts/data_preparation/prepare_ddsm_data.py
@@ -53,9 +53,6 @@ def get_parser():
 def prepare_data_ddsm(args, path_processed):
     
     # Create HDF5 file to save dataset into.
-    dir_create = os.path.dirname(args.path_create)
-    if not os.path.exists(dir_create):
-        os.makedirs(dir_create)
     f = h5py.File(args.path_create, 'w')
     f.create_group('train')
     f.create_group('valid')
@@ -80,26 +77,36 @@ def prepare_data_ddsm(args, path_processed):
                 dtype=np.uint16 if key=='s' else np.uint8,
                 **writer_kwargs)
     
-    # Index all processed DDSM files.
+    # Index all processed DDSM files. For sick cases, store path indexed 
+    # by image size. For healthy cases, simply store a list of paths.
+    print("Indexing DDSM files.")
     files_ddsm_s = {}
+    files_ddsm_s_list = []
     files_ddsm_h = []
     for root, dirs, files in os.walk(os.path.join(path_processed)):
         for fn in files:
             if not fn.endswith(".tif"):
                 # Not a processed image; skip.
                 continue
+            if fn.endswith("mask.tif"):
+                # Mask, not image; skip.
+                continue
             if root.startswith(os.path.join(path_processed, "normals")):
                 # Healthy case; store in a list.
                 files_ddsm_h.append(os.path.join(root, fn))
                 continue
-            
-            # Sick case; index.
-            fn_base = os.path.basename(fn)
-            case = re.search("(?<=^[A-Z]_)\d{4}", fn_base).group(0)
-            view = re.search("(LEFT_CC|LEFT_MLO|RIGHT_CC|RIGHT_MLO)",
-                             fn_base).group(0)
-            files_ddsm_s["{}_{}".format(case, view)] = os.path.join(root, fn)
-            #print("DEBUG {}_{}".format(case, view))
+            files_ddsm_s_list.append(os.path.join(root, fn))
+    for path in tqdm(files_ddsm_s_list):
+        # Sick case; index.
+        reader = sitk.ImageFileReader()
+        reader.SetFileName(path)
+        reader.ReadImageInformation()
+        x, y = reader.GetSize()
+        if x not in files_ddsm_s:
+            files_ddsm_s[x] = {}
+        if y not in files_ddsm_s[x]:
+            files_ddsm_s[x][y] = []
+        files_ddsm_s[x][y].append(path)
     
     # Collect and store healthy cases (resized).
     print("Processing normals (healthy) from DDSM")
@@ -113,101 +120,138 @@ def prepare_data_ddsm(args, path_processed):
         writer['train']['h'].buffered_write(im_np)
     writer['train']['h'].flush_buffer()
     
-    # Collect sick cases.
-    dirs_image = {'train': [], 'test': []}
-    dirs_mask  = {'train': defaultdict(list), 'test': defaultdict(list)}
-    keys = {'train': [], 'test': []}
+    # Identify all masks and their shapes.
+    print("Indexing CBIS-DDSM masks.")
+    paths_mask = []
     for d in sorted(os.listdir(args.path_cbis)):
-        if not os.path.isdir(os.path.join(args.path_cbis, d)):
+        d_path = os.path.join(args.path_cbis, d)
+        if not os.path.isdir(d_path):
+            # Not a directory; skip.
             continue
         if re.search("^Mass-(Training|Test).*[1-9]$", d):
-            key = re.search("\d{4}_(LEFT_CC|LEFT_MLO|RIGHT_CC|RIGHT_MLO)",
-                            d).group(0)
-            if re.search("Training", d):
-                dirs_mask['train'][key].append(d)
-                keys['train'].append(key)
-            else:
-                dirs_mask['test'][key].append(d)
-                keys['test'].append(key)
+            for root, dirs, files in os.walk(d_path):
+                for fn in files:
+                    if fn.endswith('.dcm'):
+                        path = os.path.join(root, fn)
+                        paths_mask.append(path)
+    masks_by_dir = defaultdict(list)
+    for path in tqdm(paths_mask):
+        reader = sitk.ImageFileReader()
+        reader.SetFileName(path)
+        reader.ReadImageInformation()
+        x, y, z = reader.GetSize()
+        assert(z==1)
+        root = os.path.dirname(os.path.dirname(os.path.dirname(path)))  # up 3
+        masks_by_dir[root].append((path, x, y))
     
-    # Split sick training cases into training and validation subsets.
-    keys_train = sorted(keys['train'])
+    # Match sick case masks from CBIS-DDSM to images in DDSM. Matching is done
+    # first by image size, then by view (left/right, cc/mlo), and finally
+    # by overlaying the mask with the original DDSM mask. Some sick images do 
+    # not have corresponding masks in DDSM and so they are ignored here.
+    print("Matching CBIS-DDSM masks to DDSM images.")
+    ddsm_by_cbis = {}
+    mask_by_cbis = {}
+    no_match = []
+    n_masks = 0
+    for root in tqdm(sorted(masks_by_dir.keys())):
+        common_match = None
+        masks = []
+        for path, x, y in masks_by_dir[root]:
+            n_masks += 1
+            
+            try:
+                match_ddsm = files_ddsm_s[x][y]
+            except KeyError:
+                continue    # Skip
+            
+            # Select only those with the same view.
+            candidates = []     # Tuples : DDSM image, CBIS-DDSM mask.
+            view = re.search("(LEFT_CC|LEFT_MLO|RIGHT_CC|RIGHT_MLO)",
+                             root).group(0)
+            for match in match_ddsm:
+                if re.search(view, match):
+                    candidates.append(match)
+            
+            # For each candidate image, check the overlap of the mask with the
+            # original DDSM mask corresponding to the image. Choose the image
+            # with the highest overlap > 0.
+            best_candidate = None
+            max_overlap = 0
+            for match in candidates:
+                original_mask_path = match.replace(".tif", "_mask.tif")
+                if not os.path.exists(original_mask_path):
+                    # DDSM image has no mask; skip.
+                    continue
+                mask_orig = sitk.ReadImage(original_mask_path)
+                mask_cbis = sitk.ReadImage(path)
+                overlap_filter = sitk.LabelOverlapMeasuresImageFilter()
+                overlap_filter.Execute(mask_orig, mask_cbis[:,:,0])
+                jaccard = overlap_filter.GetJaccardCoefficient()
+                if jaccard > max_overlap:
+                    max_overlap = jaccard
+                    best_candidate = match
+            if best_candidate is not None:
+                if common_match is None:
+                    common_match = best_candidate
+                if common_match!=best_candidate:
+                    raise AssertionError(
+                        "All masks in the same case should match the same "
+                        "image in the DDSM set but at least two masks match "
+                        "different images for case {}.".format(root))
+                masks.append(path)      # This is a usable mask.
+    
+        # No matches? Record this.
+        if common_match is None:
+            no_match.append(root)
+        else:
+            ddsm_by_cbis[root] = common_match
+            mask_by_cbis[root] = masks
+            n_masks += len(masks)
+    
+    # Report failed cases.
+    if len(no_match):
+        print("Skipped {} of {} masks since no matching image could be found: "
+              "\n{}".format(len(no_match),
+                            n_masks,
+                            "\n".join(no_match)))
+    
+    # Sort cases into training, validation, and test subsets.
+    train = []
+    test  = []
+    for k in mask_by_cbis.keys():
+        if re.search("Mass-Training", k):
+            train.append(k)
+        elif re.search("Mass-Test", k):
+            test.append(k)
+        else:
+            raise AssertionError("This error is not possible.")
     rng = np.random.RandomState(0)
-    rng.shuffle(keys_train)
-    n_valid = int(args.validation_fraction*len(keys_train))
-    keys_valid, keys_train = keys_train[:n_valid], keys_train[n_valid:]
-    keys = {'train': keys_train,
-            'valid': keys_valid,
-            'test' : keys['test']}
-    
-    # Create new sick cases dicts with validation subset.
-    dirs_mask_split = {
-        'train': dict([(key, dirs_mask['train'][key]) for key in keys_train]),
-        'valid': dict([(key, dirs_mask['train'][key]) for key in keys_valid]),
-        'test': dirs_mask['test']
-        }
+    rng.shuffle(train)
+    n_valid = int(args.validation_fraction*len(train))
+    cbis_dirs = {'train': train[:n_valid],
+                 'valid': train[n_valid:],
+                 'test' : test}
     
     # For each case, combine all lesions into one mask; process image 
     # and mask and store in HDF5 file.
     for fold in ['train', 'valid', 'test']:
-        print("Processing cases with masses (sick) from CBIS-DDSM : '{}' fold"
+        print("Processing cases with masses (sick) : '{}' fold"
               "".format(fold))
-        fail_list = []
-        for key in tqdm(sorted(keys[fold])):
-            mask_dir_list = dirs_mask_split[fold][key]
-            
-            # Function to get path for every dcm image in a directory tree.
-            def get_all_dcm(path):
-                dcm_list = []
-                for root, dirs, files in os.walk(path):
-                    for fn in files:
-                        if fn.endswith('.dcm'):
-                            dcm_list.append(os.path.join(root, fn))
-                return dcm_list
-            
-            # Load image, resize, and store in HDF5.
-            try:
-                im_path = files_ddsm_s[key]
-            except KeyError:
-                # The raw DDSM data is missing this case from CBIS-DDSM.
-                # (Whyyyy???)
-                fail_list.append(key)
-            im = sitk.ReadImage(im_path)
+        for root in tqdm(sorted(cbis_dirs[fold])):
+            # Load image.
+            im = sitk.ReadImage(ddsm_by_cbis[root])
             im_np = sitk.GetArrayFromImage(im)
             
-            # Load all masks, merge them together, resize, and store in HDF5.
+            # Load all usable masks, merge them together, resize,
+            # and store in HDF5.
             mask_np = None
-            for d in mask_dir_list:
-                m_np = None
-                mask_path = get_all_dcm(os.path.join(args.path_cbis, d))
-                assert len(mask_path)==2  # there should be two dcm files
-                m0 = sitk.ReadImage(mask_path[0])
-                m0_np = sitk.GetArrayFromImage(m0)
-                m1 = sitk.ReadImage(mask_path[0])
-                m1_np = sitk.GetArrayFromImage(m1)
-                # Of the two files, the mask is the one with the same
-                # image size as the full xray image.
-                assert not (    m0_np.shape==im_np.shape
-                            and m1_np.shape==im_np.shape)
-                if m0_np.shape==im_np.shape:
-                    m_np = m0_np
-                if m1_np.shape==im_np.shape:
-                    m_np = m1_np
-                if m_np is None:
-                    # Could not find a mask matching the image dimensions!
-                    # Don't bother looking at the other masks. Just fail this
-                    # case. And hope the other cases have correct masks.
-                    break
-                m_np = np.squeeze(m_np)
+            for path in mask_by_cbis[root]:
+                m = sitk.ReadImage(path)
+                m_np = np.squeeze(sitk.GetArrayFromImage(m))
                 if mask_np is None:
                     mask_np = m_np
                 else:
                     mask_np = np.logical_or(m_np, mask_np).astype(np.uint8)
-            if mask_np is None:
-                # One or more masks does not match the image dimensions.
-                # Skip this case and add it to the fail list. :(
-                fail_list.append(key)
-                continue
             im_np, mask_np = trim(im_np, mask_np)
             im_np = resize(im_np,
                            size=(args.resize, args.resize, 1),
@@ -220,13 +264,6 @@ def prepare_data_ddsm(args, path_processed):
             writer[fold]['m'].buffered_write(mask_np)
         writer[fold]['s'].flush_buffer()
         writer[fold]['m'].flush_buffer()
-    
-        # Report failed cases.
-        if len(fail_list):
-            print("Skipped {} cases due to issues with their masks (missing "
-                  "mask or incorrect size):\n{}"
-                  "".format(len(fail_list),
-                            "\n".join(fail_list)))
 
 
 def resize(image, size, interpolator=sitk.sitkLinear):
@@ -332,9 +369,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     try:
         # Convert raw cases to tiff.
+        dir_create = os.path.dirname(args.path_create)
+        if not os.path.exists(dir_create):
+            os.makedirs(dir_create)
         path_processed = args.path_processed
         if path_processed is None:
             path_processed = tempfile.mkdtemp(dir=dir_create)
+        print("Converting raw DDSM to tiff.")
         convert_raws(read_from=args.path_raws,
                      write_to=path_processed,
                      clip_noise=args.clip_noise,
