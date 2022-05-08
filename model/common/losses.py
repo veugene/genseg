b8173b2c5955b20a444de24c1626bbadddd0dd31
Eugene Vorontsov <veugene@cedar1.cedar.computecanada.ca>
2022-05-07 17:06:45 -0700

    Add andermatt-inspired infilling model.

--- model/common/losses.py
+++ model/common/losses.py
@@ -287,3 +287,21 @@ def _set_requires_grad(x, mode=True):
             _set_requires_grad(elem)
     else:
         x.requires_grad = mode
+
+
+def relevancy(segmentation, infilling, image):
+    """
+    Relevancy loss as per Andermatt et al.
+    
+    https://arxiv.org/pdf/1805.10344.pdf
+    
+    NOTE: translation is achieved by adding `infilling` to `image`.
+    """
+    assert segmentation.max() <= 1
+    SMALL = 1e-6
+    t1 = torch.log(1 - segmentation**2 + SMALL)
+    t2_num = segmentation * (image - infilling)
+    t2_den = segmentation + SMALL
+    loss = torch.norm(-t1, p=1) \
+           -torch.norm(t2_num, p=1)/torch.norm(t2_den, p=1)
+    return loss
